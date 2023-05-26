7aeb6919a7c8337769812e8d1cc1b4be0ec8cdbc
l0tk3 <1037730299@qq.com>
2023-05-26 22:57:48 +0800

    add cli1 table files

---
+++ WrokReport/week1/mugen_cli1/gen_table.py
@@ -0,0 +1,51 @@
+import json
+import os
+
+x86_log_path="./x86/logs"
+riscv_file_path="./risc-v"
+
+def isfailed(logpath):
+    logfile=open(logpath,"r+")
+    log=logfile.read()
+    logfile.close()
+    if "LOG_ERROR" in log:
+        return True
+    return False
+logdir=riscv_file_path+"/logs"
+logfailedir=riscv_file_path+"/logs_failed"
+listsfile=open(riscv_file_path+"/lists/list_test","r")
+lists=listsfile.read()
+listsfile.close()
+lists=lists.split("\n")
+# print(lists)
+table=open("table.md","w+")
+head='''| 测试套/软件包名 | 测试用例名 | 状态 | 日志文件 | 原因 |\n| :-------------: | :--------: | :--: | :------: | :--: |\n'''
+table.write(head)
+for perlist in lists:
+    suite_json=open(f"{riscv_file_path}/suite2cases/{perlist}.json","r")
+    suite=suite_json.read()
+    suite_json.close()
+    # print(suite)
+    suite=json.loads(suite)
+    # print(suite["cases"])
+    suitename=perlist
+    for case in suite["cases"]:
+        logfilelist=os.listdir(logfailedir)
+        casename=case['name']
+        status="success"
+        if perlist in logfilelist:
+            logfilelist=os.listdir(logfailedir+f"/{perlist}")
+            if casename in logfilelist:
+                status="fail"
+                x86logdir=x86_log_path+f"/{perlist}/{casename}"
+                x86logfile=os.listdir(x86logdir)[0]
+                if isfailed(x86logdir+"/"+x86logfile):
+                    status="x86 fail"
+        suitelogdir=logdir+f"/{perlist}"
+        if casename not in os.listdir(suitelogdir):
+            continue
+        thislogdir=logdir+f"/{perlist}/{casename}"
+        logfile=os.listdir(thislogdir)[0]
+        payload=f'| {suitename} | {casename} | {status} | {thislogdir+"/"+logfile} | None |\n'
+        suitename=""
+        table.write(payload)
